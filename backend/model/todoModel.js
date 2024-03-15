0656456fd6c449391bc639163677bfb76b3150ce
jeromeinfante <fyodorinfante@gmail.com>
2024-03-15 22:16:35 +0800

    server and api

---
+++ backend/model/todoModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    task:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+export default mongoose.model('Todo', TodoSchema);
+
